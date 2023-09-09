3089d596103716671870d385eb9e5ca1ea68e456
Sulaiman Qasimi <farzadqasimy@gmail.com>
2023-09-09 13:40:54 +0430

    create table for department

--- database/migrations/2023_09_09_090739_create_departments_table.php
+++ database/migrations/2023_09_09_090739_create_departments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('code')->unique()->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
