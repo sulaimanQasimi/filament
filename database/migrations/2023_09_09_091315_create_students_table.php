913202fa88fa73ededf0dbc08cf71bea8f993d29
Sulaiman Qasimi <farzadqasimy@gmail.com>
2023-09-09 14:13:07 +0430

    add english field to students table

--- database/migrations/2023_09_09_091315_create_students_table.php
+++ database/migrations/2023_09_09_091315_create_students_table.php
@@ -14,12 +14,32 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            /**
+             * University
+             */
             $table->foreignIdFor(Department::class);
             $table->string('konkor_id')->nullable();
+            $table->string('section')->nullable();
+            //$table->string('');
+            /**
+             * Language
+             */
+            $table->string('language')->nullable();
+            /**
+             * info
+             */
+            $table->string('year')->nullable();
             $table->string('name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('father_name')->nullable();
+            $table->string('grand_father_name')->nullable();
             $table->string('mother_name')->nullable();
+            /**
+             * English Field
+             */
+            $table->string('name_en')->nullable();
+            $table->string('last_name_en')->nullable();
+            $table->string('grand_father_name_en')->nullable();
             $table->timestamps();
             $table->softDeletes();
             $table->foreign('department_id')->on('departments')->references('id')->cascadeOnDelete();
